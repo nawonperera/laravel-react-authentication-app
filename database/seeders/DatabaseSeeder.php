07143146ef0888fb5f63e3200dc59200bc73a20a
Nawon <nawon2794@gmail.com>
2025-06-12 02:06:45 +0530

    Created user sign in validation part.✏️

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Nawon Perera',
+            'email' => 'nawon@example.com',
+            'password' => Hash::make('password'), // Default password
+
         ]);
     }
 }
