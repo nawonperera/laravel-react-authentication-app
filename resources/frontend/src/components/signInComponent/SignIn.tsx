5674745ce505d7c58aa7f922d6f0b900b167220f
Nawon <nawon2794@gmail.com>
2025-06-15 18:44:55 +0530

    Create form functionalities with type definitions.

--- resources/frontend/src/components/signInComponent/SignIn.tsx
+++ resources/frontend/src/components/signInComponent/SignIn.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import SignInForm from "./SignInForm.tsx";
+import type { ISignInState } from "../../utilities/types/signIn/SignIn";
+import axios from "axios";
 
 const SignIn: React.FC = () => {
+    const [signInDetails, setSignInDetails] = useState<ISignInState>({
+        email: "",
+        password: "",
+    });
+
+    const handleInputFieldChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ): void => {
+        const { name, value } = event.target; //name = "email" or "password", and value = value that we type in the input field.
+        //console.log(name, value);
+
+        setSignInDetails((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent):Promise<void> => {
+        event.preventDefault();
+        console.log(signInDetails);
+
+        // return await axios.get('/sanctum/csrf-cookie').then((res) => {
+        //     const response = axios.post("http://127.0.0.1:8000/api/sign-in", signInDetails);
+        //     console.log(response);
+        // })
+
+
+
+    };
+
     return (
-        <div>
-            SignIn Component
-        </div>
+        <SignInForm
+            handleInputFieldChange={handleInputFieldChange}
+            handleSubmit={handleSubmit}
+        />
     );
 };
 
---
+++ resources/frontend/src/components/signInComponent/SignInForm.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import type { ISingInFormProps } from "../../utilities/types/signIn/SignIn";
+
+const SignInForm: React.FC<ISingInFormProps> = ({ handleInputFieldChange, handleSubmit}) => {
+    return (
+        <section className="bg-gray-50 ">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Your email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={handleInputFieldChange}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                                    placeholder="name@company.com"
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={handleInputFieldChange}
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                                />
+                            </div>
+                            <button
+                                type="submit"
+
+                                className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                            >
+                                Sign in
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default SignInForm;
